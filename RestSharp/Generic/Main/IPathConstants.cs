51f16a5e102aebf865366e62983e4858cee23886
Varunsn9 <104562883+Varunsn9@users.noreply.github.com>
2023-03-27 13:22:35 +0530

    Const cannot be upgradable

--- RestSharp/Generic/Main/IPathConstants.cs
+++ RestSharp/Generic/Main/IPathConstants.cs
@@ -9,13 +9,13 @@ namespace RestSharp.Generic.Main
 {
     public class IPathConstants : BaseClass
     {
-
         public static string currentDirectory = Directory.GetCurrentDirectory();
         public static string parentDirectory = Directory.GetParent(Directory.GetParent(currentDirectory).FullName).FullName;
         public static string IcsvPath = Path.Combine(parentDirectory, "RestSharp","Main_Resources","CsvData.csv");
         //should be const for data source
+        //and cannot be upgradable
         public const string excelPath = @"D:\VisualStudioRepos\AzureDevOps\RestSharp\RestSharp\Main_Resources\VTiger.xlsx";
         public const string csvPath = @"D:\VisualStudioRepos\AzureDevOps\RestSharp\RestSharp\Main_Resources\CsvData.csv";
+        
     }
-
 }
